6577a398e2e645c37c13fa2a412001e2805420c2
Aayush Lama <aayushlama4008@gmail.com>
2025-06-11 18:52:10 +0200

    HERO : centered evrything

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,24 +25,22 @@ const Home = () => {
         {/* Overlay */}
         <div className="hero-overlay"></div>
 
-        {/* Content */}
-        <div className="hero-content-wrapper container-fluid">
-          <div className="row justify-content-center align-items-center text-center text-md-start">
-            <div className="col-md-8 col-lg-6 hero-text text-white">
-              <h1>
-                 Elevate Your Brand with
-                Stunning Visuals & Smart Digital Strategy
-              </h1>
-              <p className="mt-3 text-light">
-                We specialize in photography, videography, web development,
-                UI/UX design, and result-driven marketing. At Alpine Marketing
-                Studio, we don’t just build brands — we craft experiences that
-                captivate, convert, and grow your audience.
-              </p>
-              <button className="cta-button mt-4" onClick={scrollToSection}>
-                Get Started
-              </button>
-            </div>
+        {/* Centered Content */}
+        <div className="hero-content-wrapper">
+          <div className="hero-text text-white">
+            <h1>
+              Elevate Your Brand with <br />
+              Stunning Visuals & Smart Digital Strategy
+            </h1>
+            <p className="mt-3 text-light">
+              We specialize in photography, videography, web development,
+              UI/UX design, and result-driven marketing. At Alpine Marketing
+              Studio, we don’t just build brands — we craft experiences that
+              captivate, convert, and grow your audience.
+            </p>
+            <button className="cta-button mt-4" onClick={scrollToSection}>
+              Get Started
+            </button>
           </div>
         </div>
       </section>
