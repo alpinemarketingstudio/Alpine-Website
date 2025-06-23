28a4e2b85410cf79bd5090c7aa6d8ee23f2aa509
Aayush Lama <aayushlama4008@gmail.com>
2025-06-23 21:10:59 +0200

    Fixed

--- backend/alpinemarketingstudio/settings.py
+++ backend/alpinemarketingstudio/settings.py
@@ -137,4 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://alpinemarketingstudio.com",
+    "https://www.alpinemarketingstudio.com",
 ]
